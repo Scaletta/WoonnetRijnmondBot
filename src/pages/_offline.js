fa2dc69261f9ff72a774f3df1d2718cf2f6fc4e0
Scaletta <jorgos.nomikos@dialoggroep.eu>
2023-03-01 17:58:02 +0100

    Added PWA.

--- src/pages/_offline.js
+++ src/pages/_offline.js
@@ -1,1 +1,13 @@
-<h1>Page offline</h1>
+import Head from 'next/head'
+
+const Fallback = () => (
+    <>
+        <Head>
+            <title>Woonnet Rijnmond Bot</title>
+        </Head>
+        <h1>Not connected to the internet</h1>
+        <h2>Cant fetch realtime data now :(</h2>
+    </>
+)
+
+export default Fallback
