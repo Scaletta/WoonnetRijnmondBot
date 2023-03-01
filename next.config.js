160a10a5c4c3e226e18bf5a79a313c1d45474573
Scaletta <jorgos.nomikos@dialoggroep.eu>
2023-03-01 17:53:02 +0100

    Added PWA.

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
-module.exports = {
-    basePath: process.env.BASE_PATH || process.env.NODE_ENV === 'production' ? '' : '',
-    publicRuntimeConfig: {
-        modifiedDate: new Date().toISOString(),
-        maxHuur: 800,
-        minHuur: 500,
-        minReageerPositie: 30,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'www.woonnetrijnmond.nl',
-                port: '',
-                pathname: '**',
-            }
-        ]
-    }
-}
+const withPWA = require('next-pwa')({
+    dest: 'public',
+});
+module.exports =
+    withPWA({
+        basePath: process.env.BASE_PATH || process.env.NODE_ENV === 'production' ? '' : '',
+        publicRuntimeConfig: {
+            modifiedDate: new Date().toISOString(),
+            maxHuur: 800,
+            minHuur: 500,
+            minReageerPositie: 30,
+        },
+        images: {
+            remotePatterns: [
+                {
+                    protocol: 'https',
+                    hostname: 'www.woonnetrijnmond.nl',
+                    port: '',
+                    pathname: '**',
+                }
+            ]
+        }
+    });
---
+++ src/pages/_offline.js
@@ -0,0 +1,1 @@
+<h1>Page offline</h1>
